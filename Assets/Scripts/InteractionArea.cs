0a6ff84fca05e3b8f0359a3b71517ba05a8aa44c
Serenafrisman <49008981@est.ort.edu.ar>
2025-07-06 17:39:34 -0300

    .

---
+++ Assets/Scripts/InteractionArea.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractionArea : MonoBehaviour
+{
+    public GameObject UIInteractionMessage;
+    public bool canInteract;
+    public MercaderiaScript mercaderia;
+
+    private void Start()
+    {
+        UIInteractionMessage.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if (canInteract)
+            {
+                Destroy(mercaderia.gameObject);
+                EndInteraction();
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(other.gameObject.name);
+        mercaderia = other.GetComponent<MercaderiaScript>();
+        if (mercaderia)
+        {
+            UIInteractionMessage.SetActive(true);
+            canInteract = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        EndInteraction();
+    }
+
+    void EndInteraction()
+    {
+        mercaderia = null;
+        canInteract = false;
+        UIInteractionMessage.SetActive(false);
+    }
+
+}
